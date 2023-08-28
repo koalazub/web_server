0e37fb4bf4ba6df1c0a878931e69af8c76fbc8c4
Ali El Ali <7111524+koalazub@users.noreply.github.com>
2023-08-28 22:32:26 +1000

    feat: print SpaceX json data to browser as response

---
+++ api/spacex.go
@@ -0,0 +1,70 @@
+package api
+
+import (
+	"io"
+	"net/http"
+	"os"
+
+	"golang.org/x/exp/slog"
+)
+
+type LaunchServer struct {
+	launches map[string]LaunchSpecs
+}
+
+// just the basics for now and then we'll see if we can elaborate more.
+// Specs need to be updated from map[string]interface{} at some point
+type LaunchSpecs struct {
+	Fairings string                 `json:"fairings"`
+	Links    map[string]interface{} `json:"links"`
+	Reddit   map[string]interface{} `json:"reddit"`
+}
+
+func New() *LaunchServer {
+	return &LaunchServer{launches: make(map[string]LaunchSpecs)}
+}
+
+func (s *LaunchServer) HandleGetLaunches(w http.ResponseWriter, r *http.Request) {
+
+	err := GetSpaceXLaunches()
+	if err != nil {
+		slog.Error("couldn't get space launches", err)
+		return
+	}
+	file, err := os.Open("writtendata.json")
+	if err != nil {
+		slog.Error("Couldn't read the file. You sure it's there? ", err)
+		return
+	}
+	defer file.Close()
+	_, err = io.Copy(w, file)
+	if err != nil {
+		slog.Error("Issue with copying file", err)
+		return
+	}
+
+}
+
+// All the crud operations
+func GetSpaceXLaunches() error {
+	res, err := http.Get("https://api.spacexdata.com/v5/launches/upcoming")
+	if err != nil {
+		slog.Error("Something happened when requesting the launches", err)
+		return err
+	}
+	slog.Info("couldn't read", res)
+	defer res.Body.Close()
+
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		slog.Error("Couldn't read body", err)
+		return err
+	}
+
+	err = os.WriteFile("writtendata.json", body, 0644)
+	if err != nil {
+		slog.Error("Error writing to file", err)
+		return err
+	}
+	return nil
+}
--- main.go
+++ main.go
@@ -6,17 +6,26 @@ import (
 	"time"
 
 	"github.com/gorilla/mux"
+	"github.com/web-server/api"
 	"github.com/web-server/server"
 	"golang.org/x/exp/slog"
 )
 
 func main() {
+	runServer()
+}
+
+func runServer() {
+
 	r := mux.NewRouter()
 	srv := server.New()
+	lsrv := api.New()
 	addr := ":8008"
+
 	r.HandleFunc("/", srv.HandleIndex)
 	r.HandleFunc("/users/{name}", srv.HandleReadUsers)
 	r.HandleFunc("/users/create", srv.HandleCreateUser)
+	r.HandleFunc("/launches/upcoming", lsrv.HandleGetLaunches)
 	s := &http.Server{
 		Addr:           addr,
 		Handler:        r,
