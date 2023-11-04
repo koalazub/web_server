f6cc51c54a1c61adb0f1c48155d700a72535ba46
CoKoala <7111524+koalazub@users.noreply.github.com>
2023-11-05 01:15:09 +1100

    chore: prep flake for building and deployment
    Flake refactor will help with build when using CI

--- flake.nix
+++ flake.nix
@@ -6,32 +6,42 @@
     flake-parts.url = "github:hercules-ci/flake-parts";
     templ.url = "github:a-h/templ";
   };
-  outputs = inputs@{nixpkgs, flake-parts, templ, ...}: 
-    flake-parts.lib.mkFlake { inherit inputs; }{
-      systems = [
-        "x86_64-linux"
-        "x86_64-darwin"
-        "aarch64-linux"
-        "aarch64-darwin"
-      ];
-      perSystem = {pkgs, system, self', inputs', config, ... }: {
-        devShells.default = pkgs.mkShell {
-        shellHook = ''  
-        export GOPATH=$HOME/go
-        export PATH=$PATH:$GOPATH/bin
-        '';
-          nativeBuildInputs = with pkgs; [
-            go_1_21 
-            gopls 
-            go-tools 
-            gotools 
-            air
-            hurl 
-            turso-cli
-            vscode-langservers-extracted
-            # (templ.packages.${system}.templ)
-          ];
+
+  outputs = inputs@{nixpkgs, flake-parts, templ, ...}:
+    let
+      buildGoProject = pkgs: with pkgs; buildGoModule {
+        # Specify additional attributes for your Go project build
+        pname = "your-project-name";
+        version = "your-project-version";
+        src = ./.;  # assuming your Go project's source code is in the same directory as this flake file
+        vendorSha256 = null;  # set this to the correct hash for your project's vendor directory
+        nativeBuildInputs = with pkgs; [
+          go_1_21
+          go-tools
+        ];
+      };
+    in
+      flake-parts.lib.mkFlake { inherit inputs; }{
+        systems = [
+          "x86_64-linux"
+          "x86_64-darwin"
+          "aarch64-linux"
+          "aarch64-darwin"
+        ];
+        perSystem = {pkgs, system, self', inputs', config, ... }: {
+          devShells.default = pkgs.mkShell {
+            nativeBuildInputs = with pkgs; [
+              go_1_21
+              gopls
+              go-tools
+              gotools
+              air
+              hurl
+              turso-cli
+              vscode-langservers-extracted
+              # (templ.packages.${system}.templ)
+            ];
+          };
         };
       };
-    };
 }
